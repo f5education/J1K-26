550061dacbb6bfba15846b295837ceab9986db2d
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-06-02 19:36:12 -0400

    Fix indentation and uncomment namespace assignment

--- lab1-start.py
+++ lab1-start.py
@@ -3,7 +3,7 @@ def instructions():
         ######## INSERT YOUR INSTRUCTIONS BELOW THIS LINE ########
         status, ip_addr = ec2_start_instance("lee-new-arcadia")
 
-        # namespace = xc_create_namespace()       
+        namespace = xc_create_namespace()       
         # xc_create_healthcheck(namespace, "lee-hc")
         # xc_create_originpool(namespace, "lee-op", "lee-hc", ip_addr, 80)
         # xc_create_loadbalancer(namespace, "lee-lb-na", "lee-lb-na.dev.learnf5.cloud", "lee-op")
@@ -26,8 +26,8 @@ def instructions():
         'body': json.dumps({
             ######## ADJUST PARAMETERS THAT YOU WANT RETURNED TO SKILLABLE BELOW THIS LINE ########
             'status': status,
-            'ip_addr': ip_addr #,
-            # 'namespace': namespace
+            'ip_addr': ip_addr,
+            'namespace': namespace
             ######## ADJUST PARAMETERS THAT YOU WANT RETURNED TO SKILLABLE ABOVE THIS LINE ########
         })
     }

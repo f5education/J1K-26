8c7ea140a91b1969d2d2be045dce41d45a789d8a
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-06-02 17:30:30 -0400

    Implement EC2 instance start and resource setup
    Add instructions for starting an EC2 instance and configuring resources.

---
+++ lab1-start.py
@@ -0,0 +1,33 @@
+def instructions():
+    try:
+        ######## INSERT YOUR INSTRUCTIONS BELOW THIS LINE ########
+        region, instance_id = ec2_get_instance_id("lee-new-arcadia")
+        status, ip_addr = ec2_start_instance(region, instance_id)
+
+        # namespace = xc_create_namespace()       
+        # xc_create_healthcheck(namespace, "lee-hc")
+        # xc_create_originpool(namespace, "lee-op", "lee-hc", ip_addr, 80)
+        # xc_create_loadbalancer(namespace, "lee-lb-na", "lee-lb-na.dev.learnf5.cloud", "lee-op")
+        # xc_create_loadbalancer(namespace, "lee-lb-eu", "lee-lb-eu.dev.learnf5.cloud", "lee-op")
+
+        xc_create_healthcheck("default", "arcadia-na-hc")
+        xc_create_originpool("default", "arcadia-na-op", "arcadia-na-hc", ip_addr, 80)    
+        xc_create_loadbalancer("default", "aracdia-na-lb", "arcadia-na.dev.learnf5.cloud", "arcadia-na-op")     # intentionally mispelled
+        xc_create_loadbalancer("default", "aracdia-eu-lb", "arcadia-eu.dev.learnf5.cloud", "arcadia-na-op")     # intentionally mispelled
+        ######## INSERT YOUR INSTRUCTIONS ABOVE THIS LINE ########
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'errorMsg': json.dumps(str(e))
+        }
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'region': region,
+            'instance_id': instance_id,
+            'status': status,
+            'ip_addr': ip_addr
+        })
+    }

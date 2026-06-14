95e0a96e5b58b1a55dc67aade30a71a1b6058fb7
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-06-13 21:15:17 -0400

    Comment out namespace and load balancer creation

--- J1K-26-100-INTRO-start.py
+++ J1K-26-100-INTRO-start.py
@@ -2,18 +2,18 @@ def instructions():
     try:
         ######## INSERT YOUR INSTRUCTIONS BELOW THIS LINE ########
         status, ip_addr = ec2_start_instance("lee-new-arcadia")
+        namespace = xc_create_user_namespace()
+        # namespace = xc_create_namespace()
+        # xc_create_user(namespace)
+        # xc_create_healthcheck(namespace, namespace + "-hc")
+        # xc_create_originpool(namespace, namespace + "-op", namespace + "-hc", ip_addr, 80)
+        # xc_create_loadbalancer(namespace, namespace + "-lb-na", namespace + "-lb-na.dev.learnf5.cloud", namespace + "-op")
+        # xc_create_loadbalancer(namespace, namespace + "-lb-eu", namespace + "-lb-eu.dev.learnf5.cloud", namespace + "-op")
 
-        namespace = xc_create_namespace()
-        #xc_create_user(namespace)
-        xc_create_healthcheck(namespace, namespace + "-hc")
-        xc_create_originpool(namespace, namespace + "-op", namespace + "-hc", ip_addr, 80)
-        xc_create_loadbalancer(namespace, namespace + "-lb-na", namespace + "-lb-na.dev.learnf5.cloud", namespace + "-op")
-        xc_create_loadbalancer(namespace, namespace + "-lb-eu", namespace + "-lb-eu.dev.learnf5.cloud", namespace + "-op")
-
-        xc_create_healthcheck("default", "arcadia-na-hc")
-        xc_create_originpool("default", "arcadia-na-op", "arcadia-na-hc", ip_addr, 80)    
-        xc_create_loadbalancer("default", "aracdia-na-lb", "arcadia-na.dev.learnf5.cloud", "arcadia-na-op")     # intentionally mispelled
-        xc_create_loadbalancer("default", "aracdia-eu-lb", "arcadia-eu.dev.learnf5.cloud", "arcadia-na-op")     # intentionally mispelled
+        # xc_create_healthcheck("default", "arcadia-na-hc")
+        # xc_create_originpool("default", "arcadia-na-op", "arcadia-na-hc", ip_addr, 80)    
+        # xc_create_loadbalancer("default", "aracdia-na-lb", "arcadia-na.dev.learnf5.cloud", "arcadia-na-op")     # intentionally mispelled
+        # xc_create_loadbalancer("default", "aracdia-eu-lb", "arcadia-eu.dev.learnf5.cloud", "arcadia-na-op")     # intentionally mispelled
         ######## INSERT YOUR INSTRUCTIONS ABOVE THIS LINE ########
 
     except Exception as e:

aa73cfb26fd4dabb09b1c2fb03d638ab27520e53
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-06-23 22:54:39 -0400

    Change EC2 instance name in start script

--- J1K-26-100-GLOBAL-start.py
+++ J1K-26-100-GLOBAL-start.py
@@ -1,7 +1,7 @@
 def instructions():
     try:
         ######## INSERT YOUR INSTRUCTIONS BELOW THIS LINE ########
-        status, ip_addr = ec2_start_instance("lee-new-arcadia")
+        status, ip_addr = ec2_start_instance("jp-arc1-na")
         
         namespace = xc_create_user_namespace()
         

62e4ec82352d2e39b622ffbe843d84847c9ed38b
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-06-29 18:00:56 -0400

    Comment out resource creation lines
    Comment out healthcheck, originpool, and loadbalancer creation for namespaces.

--- J1K-26-100-GLOBAL-start.py
+++ J1K-26-100-GLOBAL-start.py
@@ -12,15 +12,15 @@ def instructions():
         
         namespace = xc_create_user_and_namespace()
         
-        xc_create_healthcheck(namespace, namespace + "-hc")
-        xc_create_originpool(namespace, namespace + "-op", namespace + "-hc", ip_addr, 80)
-        xc_create_loadbalancer(namespace, namespace + "-lb-na", namespace + "-lb-na.dev.learnf5.cloud", namespace + "-op")
-        xc_create_loadbalancer(namespace, namespace + "-lb-eu", namespace + "-lb-eu.dev.learnf5.cloud", namespace + "-op")
+        # xc_create_healthcheck(namespace, namespace + "-hc")
+        # xc_create_originpool(namespace, namespace + "-op", namespace + "-hc", ip_addr, 80)
+        # xc_create_loadbalancer(namespace, namespace + "-lb-na", namespace + "-lb-na.dev.learnf5.cloud", namespace + "-op")
+        # xc_create_loadbalancer(namespace, namespace + "-lb-eu", namespace + "-lb-eu.dev.learnf5.cloud", namespace + "-op")
 
-        xc_create_healthcheck("default", "arcadia-na-hc")
-        xc_create_originpool("default", "arcadia-na-op", "arcadia-na-hc", ip_addr, 80)    
-        xc_create_loadbalancer("default", "arcadia-na-lb", "arcadia-na.dev.learnf5.cloud", "arcadia-na-op")
-        xc_create_loadbalancer("default", "arcadia-eu-lb", "arcadia-eu.dev.learnf5.cloud", "arcadia-na-op")
+        # xc_create_healthcheck("default", "arcadia-na-hc")
+        # xc_create_originpool("default", "arcadia-na-op", "arcadia-na-hc", ip_addr, 80)    
+        # xc_create_loadbalancer("default", "arcadia-na-lb", "arcadia-na.dev.learnf5.cloud", "arcadia-na-op")
+        # xc_create_loadbalancer("default", "arcadia-eu-lb", "arcadia-eu.dev.learnf5.cloud", "arcadia-na-op")
         ######## INSERT YOUR INSTRUCTIONS ABOVE THIS LINE ########
 
     except Exception as e:

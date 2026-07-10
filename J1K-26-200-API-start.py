39eeea8607f95d2090b7f0d8e2a1d4f760474dde
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-07-10 11:31:02 -0400

    Fix IP address format in origin pool creation

--- J1K-26-200-API-start.py
+++ J1K-26-200-API-start.py
@@ -2,7 +2,7 @@ def instructions():
     try:
         namespace = xc_create_user_and_namespace()
         xc_create_healthcheck(namespace, namespace + "-hc")
-        xc_create_originpool(namespace, namespace + "-op", namespace + "-hc", [54.211.5.17], 80)
+        xc_create_originpool(namespace, namespace + "-op", namespace + "-hc", ["54.211.5.17"], 80)
         xc_create_loadbalancer(namespace, namespace + "-lb", namespace + "-api.f5training7.cloud", namespace + "-op")
 
     except Exception as e:

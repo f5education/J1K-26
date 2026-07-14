9b2da0347e3aff78a7cf0c07c9e18915ca476251
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-07-14 15:51:46 -0400

    Comment out Firefox snap refresh and enable commands
    Comment out Firefox snap commands for future reference.

--- 10-J1K-26.sh
+++ 10-J1K-26.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo apt install --only-upgrade google-chrome-stable
 sudo apt install --yes nmap
-sudo snap refresh firefox
-sudo snap disable firefox
-sudo snap enable firefox
+# sudo snap refresh firefox
+# sudo snap disable firefox
+# sudo snap enable firefox

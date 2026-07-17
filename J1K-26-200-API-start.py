40b94069a1d1aeddb9eff2896984413319ccbb72
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-07-16 20:59:46 -0400

    Modify instance creation and comment out resources
    Updated the instance creation template and commented out other resource creation calls.

--- J1K-26-200-API-start.py
+++ J1K-26-200-API-start.py
@@ -1,10 +1,10 @@
 def instructions():
     try:
         namespace = xc_create_user_and_namespace()
-        # ec2_create_instance('docker-template', namespace + "-app")
-        xc_create_healthcheck(namespace, namespace + "-hc")
-        xc_create_originpool(namespace, namespace + "-op", namespace + "-hc", ["54.211.5.17"], 80)
-        xc_create_loadbalancer(namespace, namespace + "-lb", namespace + "-api.f5training7.cloud", namespace + "-op")
+        ec2_create_instance('crAPI-template', namespace + "-app")
+        # xc_create_healthcheck(namespace, namespace + "-hc")
+        # xc_create_originpool(namespace, namespace + "-op", namespace + "-hc", ["54.211.5.17"], 80)
+        # xc_create_loadbalancer(namespace, namespace + "-lb", namespace + "-api.f5training7.cloud", namespace + "-op")
 
     except Exception as e:
         return {

c051932c0949960c5b4217d3c09d9938d4aff76e
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-07-18 16:39:13 -0400

    Change origin pool port from 80 to 30080

--- J1K-26-200-API-start.py
+++ J1K-26-200-API-start.py
@@ -3,7 +3,7 @@ def instructions():
         namespace = xc_create_user_and_namespace()
         _, ip_addr = ec2_create_instance('crAPI-template', namespace + "-app")
         xc_create_healthcheck(namespace, namespace + "-hc")
-        xc_create_originpool(namespace, namespace + "-op", namespace + "-hc", [ip_addr], 80)
+        xc_create_originpool(namespace, namespace + "-op", namespace + "-hc", [ip_addr], 30080)
         xc_create_loadbalancer(namespace, namespace + "-lb", namespace + "-api.f5training7.cloud", namespace + "-op")
 
     except Exception as e:

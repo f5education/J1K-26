c412c81250511f9181ce2b661c352ca3b3746119
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-07-26 14:45:44 -0400

    Update 10-J1K-26.sh

--- 10-J1K-26.sh
+++ 10-J1K-26.sh
@@ -1,7 +1,7 @@
-# quick patch until we update terminal
-sudo apt update
-sudo apt install --only-upgrade google-chrome-stable
-sudo apt install --yes nmap
+# quick patch until we update terminal - UPDATE: terminal updated
+# sudo apt update
+# sudo apt install --only-upgrade google-chrome-stable
+# sudo apt install --yes nmap
 # sudo snap refresh firefox
 # sudo snap disable firefox
 # sudo snap enable firefox
